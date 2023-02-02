bd79276ea860a299fece3b90741c87bc6c624d5f
Almeida-91 <aplalmeida91@outlook.com>
2023-02-02 22:43:08 +0000

    Add footer and firestore database

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        Developed by <a href="https://github.com/almeida-91">Almeida-91</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Waldo from "./components/game/Waldo.js";
 import LogScreen from "./components/googleSignin/logScreen";
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <LogScreen />
     <Waldo />
+    <Footer />
   </React.StrictMode>
 );
