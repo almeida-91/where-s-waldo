4351a14a84b364d3da3192239210ab678d478fe6
Almeida-91 <aplalmeida91@outlook.com>
2023-02-03 23:16:35 +0000

    Add backend check for character positions

--- src/components/game/Waldo.js
+++ src/components/game/Waldo.js
@@ -4,7 +4,7 @@ import waldo1 from "./images/waldo1.jpeg";
 import waldo2 from "./images/waldo2.jpeg";
 import waldo3 from "./images/waldo3.jpeg";
 import waldo4 from "./images/waldo4.jpeg";
-import characters from "./images/characters.webp";
+import charactersPic from "./images/characters.webp";
 import waldo from "./images/wally.webp";
 import woof from "./images/woof.webp";
 import wenda from "./images/wenda.webp";
@@ -13,13 +13,19 @@ import odlaw from "./images/odlaw.webp";
 
 import { useState } from "react";
 import "./waldo.css";
+import { getSolutions } from "./serverdata";
 
 const Waldo = () => {
   const [posX, setposX] = useState(0);
   const [posY, setposY] = useState(0);
-  const [isActive, setisActive] = useState(false);
+  const [isActive, setisActive] = useState(true);
   const [selectedImage, setSelectedImage] = useState(waldo1);
+  const [selectedChar, setSelectedChar] = useState("");
   const imageArray = [waldo1, waldo2, waldo3, waldo4];
+  const characters = ["Waldo", "Woof", "Wenda", "Whitebeard", "Odlaw"];
+  const charImages = [waldo, woof, wenda, whitebeard, odlaw];
+  const [selectedImageIndex, setSelectedImageIndex] = useState(1);
+  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
 
   const [globalMousePos, setGlobalMousePos] = useState({});
   const mouseCursor = {
@@ -27,26 +33,25 @@ const Waldo = () => {
     top: posY + "px",
   };
 
-  function submitAnswer(e) {
-    e.preventDefault();
-  }
-
   function handleMove(e) {
     let currentImage = document.getElementById("wally1");
-    document.getElementById("popup").classList.remove("showPopUp");
     setposX(e.pageX - currentImage.offsetLeft + currentImage.scrollLeft);
     setposY(e.pageY - currentImage.offsetTop + currentImage.scrollTop);
   }
 
   const handleClick = (e) => {
+    setClickPosition({ x: posX, y: posY });
     setGlobalMousePos({
       left: e.pageX + "px",
       top: e.pageY + "px",
     });
-    isActive ? setisActive(false) : setisActive(true);
-    document.getElementById("popup").classList.add("showPopUp");
-    console.log(posX, posY);
-    console.log(isActive);
+    setisActive(!isActive);
+
+    if (isActive) {
+      document.getElementById("popup").classList.add("showPopUp");
+    } else {
+      document.getElementById("popup").classList.remove("showPopUp");
+    }
   };
 
   const imageList = imageArray.map((image, index) => (
@@ -54,7 +59,10 @@ const Waldo = () => {
       key={index}
       className="imagePreview"
       src={image}
-      onClick={() => setSelectedImage(image)}
+      onClick={() => {
+        setSelectedImage(image);
+        setSelectedImageIndex(index + 1);
+      }}
       alt="game preview"
     />
   ));
@@ -69,56 +77,99 @@ const Waldo = () => {
     />
   );
 
+  const characterSelection = characters.map((character, index) => (
+    <label htmlFor={character} key={index}>
+      <p className="inputLine">
+        <input
+          type={"radio"}
+          name="charSelect"
+          id={character}
+          value={character}
+          onChange={(e) => {
+            setSelectedChar(e.currentTarget.id.toLowerCase());
+          }}
+        />{" "}
+        <img src={charImages[index]} alt={character} />
+        <span>{character}</span>
+      </p>
+    </label>
+  ));
+
   const popup = (
     <div style={globalMousePos} id="popup" className="userClick">
       <form onSubmit={submitAnswer}>
-        <label htmlFor="waldo">
-          <p className="inputLine">
-            <input type={"radio"} name="charSelect" id="waldo" />{" "}
-            <img src={waldo} alt="waldo" />
-            <span>Waldo</span>
-          </p>
-        </label>
-        <label htmlFor="woof">
-          <p className="inputLine">
-            <input type={"radio"} name="charSelect" id="woof" />{" "}
-            <img src={woof} alt="woof" />
-            <span>Woof</span>
-          </p>
-        </label>
-        <label htmlFor="wenda">
-          <p className="inputLine">
-            <input type={"radio"} name="charSelect" id="wenda" />{" "}
-            <img src={wenda} alt="wenda" />
-            <span>Wenda</span>
-          </p>
-        </label>
-        <label htmlFor="whitebeard">
-          <p className="inputLine">
-            <input type={"radio"} name="charSelect" id="whitebeard" />{" "}
-            <img src={whitebeard} alt="whitebeard" />
-            <span>Whitebeard</span>
-          </p>
-        </label>
-        <label htmlFor="odlaw">
-          <p className="inputLine">
-            <input type={"radio"} name="charSelect" id="odlaw" />{" "}
-            <img src={odlaw} alt="odlaw" />
-            <span>Odlaw</span>
-          </p>
-        </label>
+        {characterSelection}
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 
-  useEffect(() => {}, [posX, posY, isActive]);
+  async function submitAnswer(e) {
+    e.preventDefault();
+    document.getElementById("popup").classList.remove("showPopUp");
+    setisActive(false);
+    const answer = await getCharacter(selectedChar, selectedImageIndex);
+    const deltaX = clickPosition.x - answer.charCoordX;
+    const deltaY = clickPosition.y - answer.charCoordY;
+    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    if (distance < answer.charRadius) {
+      console.log("success");
+    } else {
+      console.log("nope");
+    }
+  }
 
+  async function getCharacter(character, imageindex) {
+    let answer = await getSolutions(imageindex);
+    let characterCoords;
+    switch (character) {
+      case "waldo":
+        characterCoords = {
+          charCoordX: answer.waldoX,
+          charCoordY: answer.waldoY,
+          charRadius: answer.waldoRadius,
+        };
+        break;
+      case "woof":
+        characterCoords = {
+          charCoordX: answer.woofX,
+          charCoordY: answer.woofY,
+          charRadius: answer.woofRadius,
+        };
+        break;
+      case "wenda":
+        characterCoords = {
+          charCoordX: answer.wendaX,
+          charCoordY: answer.wendaY,
+          charRadius: answer.wendaRadius,
+        };
+        break;
+      case "whitebeard":
+        characterCoords = {
+          charCoordX: answer.whitebeardX,
+          charCoordY: answer.whitebeardY,
+          charRadius: answer.whitebeardRadius,
+        };
+        break;
+      case "odlaw":
+        characterCoords = {
+          charCoordX: answer.odlawX,
+          charCoordY: answer.odlawY,
+          charRadius: answer.odlawRadius,
+        };
+        break;
+      default:
+        console.log("Error: No character selected");
+    }
+    return characterCoords;
+  }
+
+  useEffect(() => {}, []);
   return (
     <div>
       <div className="imagePreviewContainer">{imageList}</div>
       <p>Find the following characters in the picture:</p>
-      <img src={characters} alt="Characters" />
+      <img src={charactersPic} alt="Characters" />
       <div>
         X: {posX} Y: {posY}
       </div>
---
+++ src/components/game/serverdata.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { db } from "../googleSignin/config";
+import { collection, doc, setDoc, getDoc, getDocs } from "firebase/firestore";
+
+async function getSolutions(imageIndex) {
+  const docRef = doc(db, "solutions", "image" + imageIndex);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+  return docSnap.data();
+}
+
+async function getRecords(imageIndex) {
+  const docRef = doc(db, "records", "image" + imageIndex);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+  return docSnap.data();
+}
+
+export { getSolutions, getRecords };
